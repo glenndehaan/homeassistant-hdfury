bd4750cb2f62c241f7e378b95a20664e83404511
Glenn de Haan <glenn@dehaan.cloud>
2026-01-24 10:33:49 +0100

    Updated emulator

--- emulator/index.js
+++ emulator/index.js
@@ -5,60 +5,102 @@ const fs = require('fs');
 const express = require('express');
 
 /**
- * Setup Express app
+ * Express port start number
  */
-const app = express();
+const port = 3000;
 
 /**
- * Output Device
+ * Define all HDFury emulated devices
  */
-console.log(`[HDFury] Device: ${process.env.HDFURY_DEVICE}`);
+const devices = [
+    "diva",
+    "vertex2",
+    "vrroom"
+];
 
 /**
- * Trust proxy
+ * Create express app for a device
  */
-app.enable('trust proxy');
+const createEmulator = (device, key) => {
+    /**
+     * Setup Express app
+     */
+    const app = express();
+
+    /**
+     * Trust proxy
+     */
+    app.enable('trust proxy');
+
+    /**
+     * Request logger
+     */
+    app.use((req, res, next) => {
+        console.log(`[${device}][${req.method}]: ${req.originalUrl}`);
+        next();
+    });
+
+    /**
+     * Configure routers
+     */
+    app.get('/', (req, res) => {
+        res.set('Content-Type', 'text/html').send('OK');
+    });
+    if(fs.existsSync(`${__dirname}/${device}/brdinfo.json`)) {
+        app.get('/ssi/brdinfo.ssi', (req, res) => {
+            res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${device}/brdinfo.json`, 'utf8'));
+        });
+    }
+    if(fs.existsSync(`${__dirname}/${device}/cecpage.json`)) {
+        app.get('/ssi/cecpage.ssi', (req, res) => {
+            res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${device}/cecpage.json`, 'utf8'));
+        });
+    }
+    if(fs.existsSync(`${__dirname}/${device}/confpage.json`)) {
+        app.get('/ssi/confpage.ssi', (req, res) => {
+            res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${device}/confpage.json`, 'utf8'));
+        });
+    }
+    if(fs.existsSync(`${__dirname}/${device}/edidpage.json`)) {
+        app.get('/ssi/edidpage.ssi', (req, res) => {
+            res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${device}/edidpage.json`, 'utf8'));
+        });
+    }
+    if(fs.existsSync(`${__dirname}/${device}/hdrpage.json`)) {
+        app.get('/ssi/hdrpage.ssi', (req, res) => {
+            res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${device}/hdrpage.json`, 'utf8'));
+        });
+    }
+    if(fs.existsSync(`${__dirname}/${device}/infopage.json`)) {
+        app.get('/ssi/infopage.ssi', (req, res) => {
+            res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${device}/infopage.json`, 'utf8'));
+        });
+    }
+
+    /**
+     * Setup default 404 message
+     */
+    app.use((req, res) => {
+        res.status(404);
+        res.send('Not Found!');
+    });
+
+    /**
+     * Disable powered by header for security reasons
+     */
+    app.disable('x-powered-by');
+
+    /**
+     * Start listening on port
+     */
+    app.listen(port + key, '0.0.0.0', () => {
+        console.log(`[App] Running on: 0.0.0.0:${port + key} (HDFury Device: ${device})`);
+    });
+}
 
 /**
- * Request logger
+ * Setup emulators for all devices
  */
-app.use((req, res, next) => {
-    console.log(`[Web]: ${req.originalUrl}`);
-    next();
-});
-
-/**
- * Configure routers
- */
-app.get('/', (req, res) => {
-    res.set('Content-Type', 'text/html').send('OK');
-});
-app.get('/ssi/brdinfo.ssi', (req, res) => {
-    res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${process.env.HDFURY_DEVICE}/brdinfo.json`, 'utf8'));
-});
-app.get('/ssi/confpage.ssi', (req, res) => {
-    res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${process.env.HDFURY_DEVICE}/confpage.json`, 'utf8'));
-});
-app.get('/ssi/infopage.ssi', (req, res) => {
-    res.set('Content-Type', 'text/html').send(fs.readFileSync(`${__dirname}/${process.env.HDFURY_DEVICE}/infopage.json`, 'utf8'));
-});
-
-/**
- * Setup default 404 message
- */
-app.use((req, res) => {
-    res.status(404);
-    res.send('Not Found!');
-});
-
-/**
- * Disable powered by header for security reasons
- */
-app.disable('x-powered-by');
-
-/**
- * Start listening on port
- */
-app.listen(3000, '0.0.0.0', () => {
-    console.log(`[App] Running on: 0.0.0.0:3000`);
+devices.forEach((device, key) => {
+    createEmulator(device, key);
 });
